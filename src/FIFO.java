170c16d38af859268beb29b4ea6b36959fd81213
marcushovi <marcushovi@protonmail.com>
2021-11-16 12:43:09 +0100

    - buffers status added - operation status added - reading text from file added

--- src/FIFO.java
+++ src/FIFO.java
@@ -4,45 +4,56 @@ public class FIFO {
     private static String[] buffer;
     private int head;
     private int tail;
+    private String status;
 
-    public FIFO(int size) {
+
+    public FIFO( int size ) {
         head = 0;
         tail = 0;
-        buffer = new String[size];
+        buffer = new String[ size ];
+
+        int loop = buffer.length;
+        for ( int i = 0; i < loop; i++ ) {
+            buffer[ i ] = "";
+        }
     }
 
-//    public static LinkedList<String> getFifo() {
-//        return fifo;
-//    }
-//
-//    public static void setFifo(LinkedList<String> fifo) {
-//        FIFO.fifo = fifo;
-//    }
+    public void insert( String word ) {
 
-    public void insert(String word) {
-        if (tail == buffer.length) {
-            System.out.println("DATA LOST");
+        boolean isDataLost = false;
+
+        if ( tail == buffer.length ) {
             tail = 0;
+            isDataLost = true;
         }
 
-        buffer[tail] = word;
+        if ( !buffer[ tail ].isEmpty() && !buffer[ tail ].equals( "" ) )
+            isDataLost = true;
+
+
+        buffer[ tail ] = word;
         tail++;
+
+        if ( isDataLost )
+            status = "DATA LOST";
+        else
+            status = "Inserted";
     }
 
-    public String pickUp(){
+    public String pickUp() {
 
 
+        if ( head == tail ) {
 
-
-        if (head == tail) {
-            return "NO DATA";
-        }
-        else {
-            if (head == buffer.length)
+            status = "NO DATA";
+            return "---";
+        } else {
+            if ( head == buffer.length )
                 head = 0;
-            String aux = buffer[head];
-            buffer[head] = "";
+            String aux = buffer[ head ];
+            buffer[ head ] = "";
             head++;
+            status = "Picked up";
 
             return aux;
         }
@@ -51,22 +62,21 @@ public class FIFO {
     }
 
     public String toString() {
-        return Arrays.toString(buffer) + this.getHead() + " " + this.getTail();
+        return Arrays.toString( buffer ) + "\r\n H:" + this.getHead() + " T:" + this.getTail();
     }
 
     public int getHead() {
         return head;
     }
 
-    public void setHead(int head) {
-        this.head = head;
-    }
 
     public int getTail() {
         return tail;
     }
 
-    public void setTail(int tail) {
-        this.tail = tail;
+
+    public String getStatus() {
+        return status;
     }
+
 }
--- src/Karaoke.java
+++ src/Karaoke.java
@@ -1,81 +1,118 @@
 import javax.swing.*;
-import java.awt.event.KeyAdapter;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Karaoke {
 
-    private static FIFO fifo;
-    private  static JLabel label;
-    static String[] words = {"Look", "Up", "at", "the", "stars", "They're", "like", "piece", "of", "art"};
+    static String[] words;
     static int order = 0;
+    private static FIFO fifo;
+    private static JLabel labelText;
+    private static JLabel labelStatus;
+    private static JLabel labelBufferStatus;
 
     private static int generator() {
-        return (int)  Math.round( Math.random() );
+        return ( int ) Math.round( Math.random() );
     }
 
     private static void makeAction() {
 
-        switch (generator()) {
-            case 1 -> {
-                if (order <= 9)
-                    fifo.insert(words[order]);
+        switch ( generator() ) {
+            case 1:
+                if ( order > words.length - 1 )
+                    order = 0;
+
+                fifo.insert( words[ order ] );
+
                 order++;
-                System.out.println("Insert");
-            }
-            case 0 -> {
-                label.setText(fifo.pickUp());
-                System.out.println("Picked up");
-            }
+                break;
+            case 0:
+                labelText.setText( fifo.pickUp() );
+                break;
         }
+
+        labelStatus.setText( fifo.getStatus() );
+
+        labelBufferStatus.setText( fifo.toString() ); // comment this row in case you do not want to auto update displayed buffer values
+    }
+
+    public static void main( String[] args ) throws IOException {
+
+        FileReader fr = new FileReader( "src/resources/Look up at the stars - lyrics.txt" );
+        BufferedReader br = new BufferedReader( fr );
+
+        int i;
+        StringBuilder input = new StringBuilder();
+        while ( ( i = br.read() ) != -1 ) {
+            input.append( ( char ) i );
+        }
+        br.close();
+        fr.close();
+
+        // replace new line chars to a space
+        input = new StringBuilder( input.toString().trim().replaceAll( "[\\r\\n]+", " " ) );
+
+        words = input.toString().split( " " );
+
+        // size of buffer is same as number of words in txt file
+        fifo = new FIFO( words.length );
+
+        labelText = new JLabel( "Song", JLabel.CENTER );
+        labelText.setHorizontalTextPosition( JLabel.CENTER );
+        labelText.setVerticalTextPosition( JLabel.CENTER );
+        labelText.setFont( new Font( "SansSerif", Font.BOLD, 20 ) );
+
+
+        labelStatus = new JLabel( "Status", JLabel.CENTER );
+        labelStatus.setHorizontalTextPosition( JLabel.CENTER );
+        labelStatus.setVerticalTextPosition( JLabel.BOTTOM );
+
+
+        labelBufferStatus = new JLabel( "Buffer values", JLabel.CENTER );
+        labelBufferStatus.setHorizontalTextPosition( JLabel.CENTER );
+        labelBufferStatus.setVerticalTextPosition( JLabel.BOTTOM );
+
+
+        JFrame jframe = new JFrame( "Karaoke" );
+
+        jframe.addKeyListener( new MKeyListener() );
+        jframe.setLayout( new GridLayout( 3, 1 ) );
+        jframe.setSize( 800, 700 );
+
+        jframe.add( labelText );
+        jframe.add( labelStatus );
+        jframe.add( labelBufferStatus );
+
+        jframe.setVisible( true );
+
     }
 
     static class MKeyListener implements KeyListener {
 
         @Override
-        public void keyTyped(KeyEvent keyEvent) {
-
-        }
-
-        @Override
-        public void keyPressed(KeyEvent event) {
+        public void keyPressed( KeyEvent event ) {
 
             int keyCode = event.getKeyCode();
 
-            if (keyCode == KeyEvent.VK_ENTER)
+            if ( keyCode == KeyEvent.VK_ENTER )
                 makeAction();
 
-            if (keyCode == KeyEvent.VK_SPACE)
-                System.out.println(fifo.toString());
+            if ( keyCode == KeyEvent.VK_SPACE )
+                labelBufferStatus.setText( fifo.toString() );
         }
 
         @Override
-        public void keyReleased(KeyEvent keyEvent) {
+        public void keyTyped( KeyEvent keyEvent ) {
+        }
 
+        @Override
+        public void keyReleased( KeyEvent keyEvent ) {
         }
     }
 
-
-    public static void main(String[] args) {
-        String[] words = {"Look", "Up", "at", "the", "stars", "They're", "like", "piece", "of", "art"};
-
-        fifo = new FIFO(10);
-
-        label = new JLabel("Song",JLabel.CENTER);
-        label.setHorizontalTextPosition(JLabel.CENTER);
-        label.setVerticalTextPosition(JLabel.CENTER);
-
-
-        JFrame jframe = new JFrame("Karaoke");
-
-        jframe.addKeyListener(new MKeyListener());
-
-        jframe.add(label);
-
-        jframe.setSize(400, 350);
-
-        jframe.setVisible(true);
-
-    }
 
 }
